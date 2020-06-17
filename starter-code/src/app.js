c407817692252bbb63c0da784c7d3cb60030ad96
sisidjidji <s.azibi@live.fr>
2020-06-17 13:38:05 -0500

    change form name from form to index

--- starter-code/src/app.js
+++ starter-code/src/app.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
 import './app.scss';
-
-// Let's talk about using index.js and some other name in the component folder
-// There's pros and cons for each way of doing this ...
 import Header from './components/header';
 import Footer from './components/footer';
-import Form from './components/form/form.js';
+import Form from './components/form';
 
 class App extends React.Component {
   render() {
