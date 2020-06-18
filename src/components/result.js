c9d4ba3564b1a4cfb594c41a48de03dc5984caef
sisidjidji <s.azibi@live.fr>
2020-06-18 08:12:13 -0500

    result.js done'

--- src/components/result.js
+++ src/components/result.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import JSONPretty from 'react-json-pretty';
 
-var JSONPretty = require('react-json-pretty');
 
+var JSONPrettyMon = require('react-json-pretty/dist/acai');
+
+export default  defaultResault(prop)
+{
+return(
+<JSONPretty id="json-pretty" data={prop.body}> theme={JSONPrettyMon} </JSONPretty>
+)
+}
+
+ 
